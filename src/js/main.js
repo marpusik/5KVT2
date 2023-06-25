986491bbfceb35b67a61d7a399d35445885aeeeb
marpusik <marpusik1987@gmail.com>
2023-06-25 14:24:29 +0300

    54

--- src/js/main.js
+++ src/js/main.js
@@ -111,12 +111,12 @@ function accordion() {
             if (parent.classList.contains('accordion__item-active')) {
                 parent.classList.remove('accordion__item-active')
             }
-            // else {
-            //     document
-            //         .querySelectorAll('.accordion__item')
-            //         .forEach(child => child.classList.remove('accordion__item-active'))
-            //     parent.classList.add('accordion__item-active')
-            // }
+            else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))
+                parent.classList.add('accordion__item-active')
+            }
         })
     })
 }
